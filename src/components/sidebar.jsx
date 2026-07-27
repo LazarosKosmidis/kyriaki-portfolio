07a19f1483431b712b4d80049b90514b87444a50
Lazik <lazaros.kosmidis@yahoo.com>
2026-07-27 11:21:59 +0300

    Refactor code

---
+++ src/components/sidebar.jsx
@@ -0,0 +1,21 @@
+
+function sidebar() {
+
+
+    return(
+        <div className="sidebar">
+        
+                        <div className="sidebar-label">
+                            DATA ENGINEER
+                        </div>
+        
+                        <div className="sidebar-number">
+                            01
+                        </div>
+        
+                </div>
+    );
+
+}
+
+export default sidebar;
